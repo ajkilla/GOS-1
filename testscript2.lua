68c6e9791dbe63b4dae5475e3a87ae7054a5c74f
DrakeSharp <krystian1876@gmail.com>
2015-07-24 20:31:48 +0200

    1

--- testscript2.lua
+++ testscript2.lua
@@ -1,3 +1,4 @@
+local version = 1.34
 OnLoop(function(myHero)
 DrawText(string.format("currhp = %f", GetCurrentHP(myHero)),12,0,80,0xff00ff00);
 end)
--- testscript3.lua
+++ testscript3.lua
@@ -1,23 +1,29 @@
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
+local Updater = {} 
+Updater.__index = Updater
+function Updater.create(address, version)
+   local self = setmetatable({}, Updater)
+   self.address=address
+   self.version=version
+   return self
+end
+
+function Updater:isUpToDate()
+  local https = require 'ssl.https'
+  local resp = {}
+  local r, c, h, s = https.request{
+    url = self.address,
+    sink = ltn12.sink.table(resp),
+    protocol = "tlsv1"
+  }
+  self.response=resp[1]
+  local ServerVersion = tonumber(string.match(ServerData, "local version = \"%d+.%d+\""))
+  return ServerVersion
 end
 
 
 
-local https = require 'ssl.https'
-local resp = {}
-local r, c, h, s = https.request{
-    url = "https://github.com",
-    sink = ltn12.sink.table(resp),
-    protocol = "tlsv1"
-}
-print (dump(resp))
+-- create and use an Account
+acc = Updater.create("https://raw.githubusercontent.com/DrakeSharp/GOS/master/testscript2.lua", 1.2)
+print (acc:isUpToDate())
+
+
