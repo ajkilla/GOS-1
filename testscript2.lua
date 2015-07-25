2edb31e56fb4bd25a406acfd99c2a631fe321e43
DrakeSharp <krystian1876@gmail.com>
2015-07-25 20:38:06 +0200

    u

--- testscript2.lua
+++ testscript2.lua
@@ -1,9 +1,9 @@
-local version = 1.2
+local version = 1.21
 
 u=require("Updater")
 up=u.create("https://raw.githubusercontent.com/DrakeSharp/GOS/master/testscript2.lua", "testscript2", version)
 if up:newVersion() then up:update() end
 
 OnLoop(function(myHero)
-DrawText(string.format("currhp = %f", GetCurrentHP(myHero)),12,0,80,0xFF0F47FF);
+DrawText(string.format("currhp = %f", GetCurrentHP(myHero)),12,0,80,0xFFFF0FBF);
 end)
